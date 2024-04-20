c7b079c6e45e0408ea7d2965953871de89acbb7a
Demiurrg <forge0047@gmail.com>
2024-04-20 04:11:48 +0300

    Lab 3: News Main Page

---
+++ vue/src/components/pages/buletin/News.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="news">
+    <PageHeader />
+    <UselessBanner />
+    <FirstCard
+      :currentNews="latestNews"
+    />
+    <SearchSection @search="(data) => handleSearch(data)" />
+    <div class="news__grid">
+      <SecondCard
+        v-for="news in filteredNews"
+        :key="news.id"
+        :id="news.id"
+        :image="news.image"
+        :date="news.date"
+        :title="news.title"
+        :description="news.description"
+      />
+    </div>
+    <PageFooter />
+  </div>
+</template>
+
+<script>
+import PageHeader from "@/components/pages/buletin/components/PageHeader.vue";
+import FirstCard from "@/components/pages/buletin/components/BigNewsType.vue";
+import SecondCard from "@/components/pages/buletin/components/SmallNewsType.vue";
+import PageFooter from "@/components/pages/buletin/components/PageFooter.vue";
+import UselessBanner from "@/components/pages/buletin/components/UselessBanner.vue";
+import { mapGetters } from "vuex";
+import SearchSection from "@/components/pages/buletin/components/SearchSection.vue";
+
+export default {
+  name: "NewsPage",
+  components: {
+    SearchSection,
+    PageFooter,
+    PageHeader,
+    FirstCard,
+    SecondCard,
+    UselessBanner
+  },
+  data () {
+    return {
+      currentSlide: 0,
+      inputData: ""
+    };
+  },
+  computed: {
+    ...mapGetters("newsStore", [
+      "getNews",
+      "getFilteredNews",
+    ]),
+    allNews () {
+      return this.getNews;
+    },
+    filteredNews () {
+      return this.getFilteredNews(this.inputData);
+    },
+    latestNews () {
+      return this.allNews[0];
+    }
+  },
+  methods: {
+    handleSearch (data) {
+      this.inputData = data;
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.news {
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  padding: 0 235px;
+
+  &__grid {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    gap: 16px;
+  }
+}
+</style>
