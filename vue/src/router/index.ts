41ec4d7ad14af38afc268529b550112f9d9bfe75
Demiurrg <forge0047@gmail.com>
2024-04-21 23:59:12 +0300

    Lab 3: Fit blogs branch

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -2,20 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomePage from '../components/pages/Home.vue'
 import {RouteNames} from './routes'
-import Button8thPage from '@/components/pages/Button8thPage.vue'
-import NinthButtonPage from "@/components/pages/NinthButtonPage.vue";
-import SecondButtonPage from "@/components/pages/SecondButtonPage.vue";
-import FirstButtonPage from "@/components/pages/FirstButtonPage.vue";
-import TenthButtonPage from "@/components/pages/TenthButtonPage.vue"
-import ButtonSixthPage from "@/components/pages/SixthButtonPage.vue"
-import FourthButtonPage from "@/components/pages/FourthButtonPage.vue";
-import TwelfthButtonPage from '../components/pages/TwelfthButtonPage.vue'
-import FourteenthButtonPage from '../components/pages/FourteenthButtonPage.vue'
-import ThirdButtonPage from '../components/pages/ThirdButtonPage.vue'
-import FifthButtonPage from "@/components/pages/FifthButtonPage.vue";
-import SeventhButtonPage from '@/components/pages/SeventhButtonPage.vue';
-import ThirteenButtons from "@/components/pages/ThirteenButtons.vue";
-import EleventhPage from "@/components/pages/EleventhPage.vue";
 import News from '@/components/pages/buletin/News.vue';
 import Article from "@/components/pages/buletin/Article.vue";
 import PostArticle from '@/components/pages/buletin/PostArticle.vue';
@@ -27,76 +13,6 @@ const routes = [
     path: '/',
     name: RouteNames.HOME,
     component: HomePage
-  },
-  {
-    path: '/second_button',
-    name: RouteNames.SECOND_BUTTON,
-    component: SecondButtonPage
-  },
-  {
-    path: '/button_8',
-    name: RouteNames.BUTTON_8,
-    component: Button8thPage
-  },
-  {
-    path: '/ninth_button',
-    name: RouteNames.NINTH_BUTTON,
-    component: NinthButtonPage
-  },
-  {
-    path: '/thirteenth_button',
-    name: RouteNames.THIRTEENTH_BUTTON,
-    component: ThirteenButtons
-  },
-  {
-    path: '/button_1',
-    name: RouteNames.BUTTON_1,
-    component: FirstButtonPage
-  },
-  {
-    path: '/tenth_button',
-    name: RouteNames.TENTH_BUTTON,
-    component: TenthButtonPage
-  },
-  {
-    path: '/eleventh-button',
-    name: RouteNames.ELEVENTH_BUTTON,
-    component: EleventhPage
-  },
-  {
-    path: '/fourth_button',
-    name: RouteNames.FOURTH_BUTTON,
-    component: FourthButtonPage
-  },
-  {
-    path: '/sixth_button',
-    name: RouteNames.SIXTH_BUTTON,
-    component: ButtonSixthPage
-  },
-  {
-    path: '/twelfth_button',
-    name: RouteNames.TWELFTH_BUTTON,
-    component: TwelfthButtonPage
-  },
-  {
-    path: '/fourteenth_button',
-    name: RouteNames.FOURTEENTH_BUTTON,
-    component: FourteenthButtonPage
-  },
-  {
-    path: '/third_button',
-    name: RouteNames.THIRD_BUTTON,
-    component: ThirdButtonPage
-  },
-  {
-    path: '/fifth_button',
-    name: RouteNames.FIFTH_BUTTON,
-    component: FifthButtonPage
-  },
-  {
-    path: '/seventh_button',
-    name: RouteNames.SEVENTH_BUTTON,
-    component: SeventhButtonPage
   },
   {
     path: '/news',
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,17 +1,4 @@
 import modals from './modals'
-import firstButton from './firstButton'
-import ThirteenthButtonStore from "./ThirteenthButtonStore";
-import twelfthButton from './twelfthButton'
-import fourthButton from './fourthButton';
-import secondButton from './secondButton'
-import thirdButton from './thirdButton'
-import sixthButton from './sixthButton'
-import tenthButton from './tenthButton'
-import fifthButton from './fifthButton';
-import fourteenthButton from './fourteenthButton';
-import eighthButton from './eighthButton'
-import NinthButton from "./NinthButton";
-import seventhButton from './seventhButton'
 import newsStore from '@/store/storeBuletin/newsStore'
 
 export default {
@@ -21,19 +8,6 @@ export default {
   mutations: {},
   modules: {
     modals,
-    newsStore,
-    firstButton,
-    NinthButton,
-    ThirteenthButtonStore,
-    twelfthButton,
-    secondButton,
-    thirdButton,
-    fourthButton,
-    sixthButton,
-    tenthButton,
-    fifthButton,
-    fourteenthButton,
-    eighthButton,
-    seventhButton
+    newsStore
   }
 }
